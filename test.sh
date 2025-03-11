c4bc4c3e762617ea6e56bcbe483def17eabe132b
elv-joeA <joe.anakata@eluv.io>
2025-03-11 20:55:08 +0000

    test script for ocr

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+[ "$ELV_MODEL_TEST_GPU_TO_USE" != "" ] || ELV_MODEL_TEST_GPU_TO_USE=0
+
+set -x
+
+rm -rf test_output/
+mkdir test_output
+
+mkdir -p .cache-ro
+
+podman run --rm --volume=$(pwd)/test:/elv/test:ro --volume=$(pwd)/test_output:/elv/tags --volume=$(pwd)/.cache-ro:/root/.cache:ro --network host --device nvidia.com/gpu=$ELV_MODEL_TEST_GPU_TO_USE ocr test/test.jpg
+
+ex=$?
+
+cd test_output
+
+exit $ex
