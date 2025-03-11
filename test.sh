b087156bc8e033ddab969218dc50edb594c74b25
elv-joeA <joe.anakata@eluv.io>
2025-03-11 20:57:58 +0000

    we do need a read-write cache for ocr

--- test.sh
+++ test.sh
@@ -7,9 +7,9 @@ set -x
 rm -rf test_output/
 mkdir test_output
 
-mkdir -p .cache-ro
+mkdir -p .cache
 
-podman run --rm --volume=$(pwd)/test:/elv/test:ro --volume=$(pwd)/test_output:/elv/tags --volume=$(pwd)/.cache-ro:/root/.cache:ro --network host --device nvidia.com/gpu=$ELV_MODEL_TEST_GPU_TO_USE ocr test/test.jpg
+podman run --rm --volume=$(pwd)/test:/elv/test:ro --volume=$(pwd)/test_output:/elv/tags --volume=$(pwd)/.cache:/root/.cache --network host --device nvidia.com/gpu=$ELV_MODEL_TEST_GPU_TO_USE ocr test/test.jpg
 
 ex=$?
 
